8a96f68e67ce77a2e022b2f988035c52480877d7
LiuYong <793875613@qq.com>
2022-04-27 09:57:31 +0800

    commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gohutool/boot4go-util
+
+go 1.18
+
+require github.com/valyala/fastjson v1.6.3
---
+++ util4go.go
@@ -0,0 +1,13 @@
+package util4go
+
+/**
+* golang-sample源代码，版权归锦翰科技（深圳）有限公司所有。
+* <p>
+* 文件名称 : util4go.go
+* 文件路径 :
+* 作者 : DavidLiu
+× Email: david.liu@ginghan.com
+*
+* 创建日期 : 2022/4/27 09:51
+* 修改历史 : 1. [2022/4/27 09:51] 创建文件 by LongYong
+*/
