a0e704fe9ce2cf18c113604a836dd62017b19c95
LiuYong <793875613@qq.com>
2022-06-02 13:57:35 +0800

    add get and test with generic

--- byte.go
+++ byte.go
@@ -3,6 +3,8 @@ package util4go
 import (
 	"bytes"
 	"encoding/binary"
+	"reflect"
+	"unsafe"
 )
 
 /**
@@ -56,4 +58,25 @@ func BytesToIntEndian[T int | int8 | int16 | int32 | int64 | uint | uint8 | uint
 		binary.Read(buf, binary.LittleEndian, data)
 	}
 	return T(*data)
+}
+
+// BytesToString converts byte slice to a string without memory allocation.
+//
+// Note it may break if the implementation of string or slice header changes in the future go versions.
+func BytesToString(b []byte) string {
+	/* #nosec G103 */
+	return *(*string)(unsafe.Pointer(&b))
+}
+
+// StringToBytes converts string to a byte slice without memory allocation.
+//
+// Note it may break if the implementation of string or slice header changes in the future go versions.
+func StringToBytes(s string) (b []byte) {
+	/* #nosec G103 */
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	/* #nosec G103 */
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+
+	bh.Data, bh.Len, bh.Cap = sh.Data, sh.Len, sh.Len
+	return b
 }
